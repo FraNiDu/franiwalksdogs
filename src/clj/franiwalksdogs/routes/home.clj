15e9ce80642326ce52df32c524f805469f2868de
daplay <daplay@gmail.com>
2019-01-13 21:22:53 -0800

    turns everything into a blankpage

--- src/clj/franiwalksdogs/routes/home.clj
+++ src/clj/franiwalksdogs/routes/home.clj
@@ -1,34 +1,14 @@
 (ns franiwalksdogs.routes.home
   (:require [franiwalksdogs.layout :as layout]
-            [franiwalksdogs.validation :refer [get-errors?]]
-            [franiwalksdogs.email :as email]
-            ;; [franiwalksdogs.db.core :as db]
             [selmer.parser :as selmer]
-            [compojure.core :refer [defroutes GET POST]]
+            [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render "home.html"))
 
-(defn do-contact [payload]
-  (let [html (selmer/render-file "mails/contact.html" payload)]
-    (email/submit 
-      "franiwalksdogs@gmail.com" 
-      "Nuevo contacto en franiwalksdogs.com" 
-      html)))
-
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (POST "/contact" [name email message captcha :as request]
-        (let [payload {:name name
-                       :email email
-                       :message message
-                       :captcha captcha}]
-          (if-let [errors (get-errors? payload)]
-            (response/bad-request errors)
-						(do
-              (do-contact payload)
-							(response/ok {:status :ok}))))))
+  (GET "/" [] (home-page)))
 
 
